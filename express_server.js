b00e2961a1fe01982a37b555481286d094ccc594
Eric Bradshaw <37272270+EricBradshaw98@users.noreply.github.com>
2024-01-22 17:52:13 -0500

    Update express_server.js

--- express_server.js
+++ express_server.js
@@ -9,10 +9,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on poirt ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
