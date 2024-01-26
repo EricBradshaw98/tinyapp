c8ab095683164d9df2dec3b0740c4c1a4f109a26
Eric Bradshaw <37272270+EricBradshaw98@users.noreply.github.com>
2024-01-25 19:33:03 -0500

    added helpers.js

---
+++ helpers.js
@@ -0,0 +1,15 @@
+
+
+const getUserByEmail = function(email, users) {
+  for (const userId in users) {
+    if (users[userId].email === email) {
+      const user = users[userId];
+      return user
+    }
+  }
+  return null;
+};
+
+
+module.exports =  getUserByEmail 
+
